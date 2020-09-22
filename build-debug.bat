2bf8b94e6aeb6d13fc2750fd0974300f8177ce7a
Geraldo Fada <geraldofada.neto@gmail.com>
2020-09-22 16:53:39 -0300

    build-debug comments

--- build-debug.bat
+++ build-debug.bat
@@ -1,14 +1,14 @@
 @echo off
 
 REM referencia dos switchs do cl
-REM  -nologo, remove o logo do msvc
-REM      -Zi, cria o arquivo .pdb para debug (caso der algum problema olhar o -Z7)
-REM      -W4, nivel de erro alto próximo ao Wall
-REM      -WX, tratar avisos como erro
-REM      -MT, linkar estaticamente o arquivo .lib de thread (evita problemas de compatibilidade)
-REM      -FC, na hora de compilar mostra o caminho absoluto dos arquivos
-REM    /link, passa opções pro linker
-REM -opt:ref, tenta remover o máximo opções não utilizadas nos arquivos map e debug
+REM  -nologo, remove the msvc logo
+REM      -Zi, creates a .pdb file used by the debbuger (if any error occurs look the -Z7)
+REM      -W4, high error level close to Wall
+REM      -WX, warnings are erros
+REM      -MT, static link the thread lib to avoid compatibility issues (default is -MD)
+REM      -FC, show the absolute path when compiling
+REM    /link, give the linker options
+REM -opt:ref, eliminates functions and data that are never referenced
 
 
 if not exist "build/debug/x64" mkdir "build/debug/x64"
